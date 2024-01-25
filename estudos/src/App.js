e566626696f2217181f52318aeee16a1ee07890d
gasperpb <achillespb@hotmail.com>
2024-01-24 22:56:51 -0300

    react-hook-form

--- estudos/src/App.js
+++ estudos/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Form from "./pages/formhook";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Form />
     </div>
   );
 }
