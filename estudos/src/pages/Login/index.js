98431fe413db41ad4456f5cb237c7ba07cb83bac
gasperpb <achillespb@hotmail.com>
2024-01-30 22:49:25 -0300

    componentes

--- estudos/src/pages/Login/index.js
+++ estudos/src/pages/Login/index.js
@@ -1,20 +1,37 @@
 import React from "react";
-import { Container, Form } from "./styles";
+import { Container, Form, SubContainerSign } from "./styles";
 import { InputCustomizado } from "../../components/ImputCustomizado";
+import Botao from "../../components/Botao";
 
 const Login = () => {
   return (
-    <div className="inputbox">
-      <InputCustomizado type="text" placeholder="Nome" />
-      <span>Mone</span>
-      <InputCustomizado type="text" placeholder="Email" />
-      <span>Email</span>
-      <InputCustomizado type="password" placeholder="Senha" />
-      <span>Senha</span>
-      <InputCustomizado type="password" placeholder="Confirmar Senha" />
-      <span>Confirmar Senha</span>
-      <button type="submit">Cadastrar</button>
-    </div>
+    <Container>
+      <Form>
+        <h1>Faça o seu Login 👋</h1>
+        <InputCustomizado
+          name="email"
+          placeholder="Digite o seu e-mail"
+          onChange={handleChange}
+          type="email"
+        />
+        <InputCustomizado
+          name="password"
+          placeholder="Digite a sua senha"
+          onChange={handleChange}
+          type="password"
+        />
+        <Botao
+          type="submit"
+          text="Entrar!"
+          onClick={handleSubmit}
+          disabled={loading === true || !validadorInput()}
+        />
+        <SubContainerSign>
+          <p>Não possui conta?</p>
+          <NavLink to="cadastrar">Cadastrar</NavLink>
+        </SubContainerSign>
+      </Form>
+    </Container>
   );
 };
 
